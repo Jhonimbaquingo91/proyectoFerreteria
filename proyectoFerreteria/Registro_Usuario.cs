fd1c216d721f9e798185d452e819aed1d423ccf3
LESLIE PAOLA TOLAGASI SAAVEDRA <ltolagasu@gmail.com>
2021-07-27 17:39:06 -0500

    Registro Usuario

---
+++ proyectoFerreteria/Registro_Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoFerreteria
+{
+    public partial class Registro_Usuario : Form
+    {
+        public Registro_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
